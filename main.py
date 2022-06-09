21dfb9b3d420b0ec964cad9ce1ebf6a807ba8ec5
Botdvp <91675155+Botdvp@users.noreply.github.com>
2022-06-09 14:11:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import pyshorteners
 
-bitly_token = '38b6f0702be7bd7656ac1dab614c3598e6558344'
 bot = telebot.TeleBot(os.getenv('TgBOT_TOKEN'))
 
 markup = InlineKeyboardMarkup()
@@ -25,7 +24,7 @@ def welcome_msg(message):
 @bot.message_handler(func = lambda msg: True)
 def make_short(msg):
     link = msg.text
-    shortener = pyshorteners.Shortener(api_key = bitly_token)
+    shortener = pyshorteners.Shortener(api_key = os.getenv('BITLY_TOKEN'))
     link_shortener = shortener.bitly.short(link)
     print(link_shortener)
     bot.reply_to(msg,link_shortener)
